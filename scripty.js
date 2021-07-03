686b04b9a071c51328a5cf803e867c74ef23d7c6
teehink <60492251+teehink@users.noreply.github.com>
2021-07-03 12:02:20 -1000

    Add files via upload

---
+++ scripty.js
@@ -0,0 +1,39 @@
+const pics = document.getElementsByClassName("current-pic")
+const totalPics = pics.length
+let picPosition = 0
+
+const nextBtn = document.getElementById("next-btn")
+const prevBtn = document.getElementById("prev-btn")
+
+nextBtn.addEventListener("click", moveToNextPic )
+prevBtn.addEventListener("click", moveToPrevPic )
+
+
+function hidePics() {
+  for (let pic of pics) {
+    pic.classList.remove("show-pic")
+    pic.classList.add("hide-pic")
+  }
+}
+
+function moveToNextPic() {
+  hidePics()
+  if (picPosition = totalPics - 1) {
+    picPosition === 0
+  } else {
+    picPosition ++
+  }
+  pics[picPosition].classList.remove("hide-pic")
+  pics[picPosition].classList.add("show-pic")
+}
+
+function moveToPrevPic() {
+  hidePics()
+  if (picPosition === 0) {
+    picPosition = totalPics - 1
+  } else {
+    picPosition --
+  }
+  pics[picPosition].classList.remove("hide-pic")
+  pics[picPosition].classList.add("show-pic")
+}
